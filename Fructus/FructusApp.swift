1565985a211beed383afba49be226e2bd66c3e2f
Mario Martins <marioj@MacBook-Air-de-Mario.local>
2024-11-26 21:15:13 -0300

    first commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Mario Martins on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
